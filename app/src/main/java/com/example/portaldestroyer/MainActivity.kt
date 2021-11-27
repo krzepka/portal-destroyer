b87d4ac74c10dcdf1e908229242f5006cb758d9b
krzepka <rzarol.kepka@gmail.com>
2021-11-27 14:16:44 +0100

    add loading 3d model, based on sceneform halloween example

--- app/src/main/java/com/example/portaldestroyer/MainActivity.kt
+++ app/src/main/java/com/example/portaldestroyer/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.portaldestroyer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.commit
+import com.google.ar.sceneform.ux.ArFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.main_activity)
+
+        setSupportActionBar(findViewById<Toolbar>(R.id.toolbar).apply {
+            title = ""
+        })
+
+        supportFragmentManager.commit {
+            add(R.id.containerFragment, MainFragment::class.java, Bundle())
+        }
     }
 }
+
